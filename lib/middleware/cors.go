af1257dbd85503164fb20a9ef0791cce04950dfb
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2020-10-06 11:23:12 +0700

    change allow origin CORS

--- lib/middleware/cors.go
+++ lib/middleware/cors.go
@@ -1,13 +1,22 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 // CORS for website
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		log.Printf("Request Host: %s", c.Request.Host)
+		switch c.Request.Host {
+		case "http://localhost:3000", "https://staging-binadesa.weeber.id", "https://telukjambe.id":
+			c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Host)
+		}
+
 		c.Writer.Header().Set("Content-Type", "application/json")
 		// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000, https://staging-binadesa.weeber.id, https://telukjambe.id")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
