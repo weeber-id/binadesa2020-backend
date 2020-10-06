6043aef4805b479c75caf20eec6c0e204cffe252
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2020-10-06 11:08:24 +0700

    change allow origin CORS

--- lib/middleware/cors.go
+++ lib/middleware/cors.go
@@ -6,7 +6,8 @@ import "github.com/gin-gonic/gin"
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Content-Type", "application/json")
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000, https://staging-binadesa.weeber.id, https://telukjambe.id")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
